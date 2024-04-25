c4f17e2c72e0eecf2a539bbb53e684e777c2414f
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-04-25 19:16:39 +0200

    work

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,10 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
-  pickCard(){
-    this.pickCardAnimation = true;
+  currentCard: string = '';
+  public game: Game = new Game();
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  pickCard() {
+    if (this.game && this.game.stack.length > 0) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      console.log('New card:', this.currentCard);
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
